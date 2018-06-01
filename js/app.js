3406502f29cf6929e2d66d831b9b373872131db7
LoreVazquez <lorena_vor@hotmail.com>
2018-06-01 11:30:04 -0500

    Se agrega la funcionalidad para obtener las estudiantes activas e inactivas por sede

--- js/app.js
+++ js/app.js
@@ -7,52 +7,32 @@ var cdmx = data['CDMX'];
 var lim = data['LIM'];
 var scl = data['SCL'];
 
-/*// Variables para obtener un array de la data de las estudiante por generacion
-var genAqp = Object.keys(aqp);
-var genCdmx = Object.keys(cdmx);
-var genLim = Object.keys(lim);
-var genScl = Object.keys(scl)
+// Variable para guardar la suma de las estudiantes activas e inactivas por sede
+var active = 0;
+var inactive = 0;
 
-var nombre = aqp[genAqp[0]]
-var prueba = Object.keys(nombre)
-console.log(prueba);
+// Iteracion para ingresar a los datos por generacion
+for (var i in aqp){
+	console.log(aqp[i])
+// Iteracion para ingresar a los datos de los estudiantes y ratings	
+	for (var k in aqp[i]){
+		console.log(aqp[i][k])
 
+// Variable para guardar el array de estudiantes por generacion
+	var array = aqp[i][k]
 
-var infoGenAqp = []
-for (i=0; i < genAqp.length; i++){
-	var infoGen = (Object.keys(aqp[genAqp[i]]));
-
-	for(j=0; j < infoGen.length; j++){
-	}*/
-
-
-
-// console.log(sedes);
-
-/* var generaciones = data.AQP; 
-// console.log(generaciones);
-
-var properties = Object.keys(generaciones);
-var valor = properties[0];
-
-
-// console.log(properties);
-
-var newArray = [
-	[valor, generaciones[valor]],
-	[properties[1], generaciones[properties[1]]]
-];
-
-
-var est = data.AQP["2016-2"].students[0];
-
-
-var activo = newArray[0][1].students[0].active;
-
-console.log(activo);
-
-console.log(newArray);
-// console.log(generaciones[properties[1]); */
-
-
-
+// Iteracion para entrar a la informacion de cada estudiante y obtener el valor de active.
+		array.forEach(function (element,index) {
+			var activeStatus = array[index].active
+			console.log(activeStatus)
+			if( activeStatus === true){
+				active += 1
+			} else if(activeStatus === false ){
+				inactive += 1
+			}
+//Muestra la suma total de las estudiantes activas e inactivas
+			console.log(active)
+			console.log(inactive)
+		});
+	}
+}
