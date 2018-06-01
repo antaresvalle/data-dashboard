b009a3882c8f20705500d6d64bdb41acf80f8657
antaresvalle <antaresvll561@gmail.com>
2018-06-01 10:22:45 -0500

    se agrega la funcionalidad del boton entrar y se vinvula el archivo overview.js al overview.html

--- js/app.js
+++ js/app.js
@@ -1,56 +1,8 @@
-// sedes
+// Boton entrar
 
-var arequipa = data.AQP;
-var cdmx = data.CDMX;
-var lima = data.LIM;
-var santiago = data.SCL;
+var entrar = document.getElementById('btn-entrar');
+console.log(entrar);
 
-
-// GENERACIONES
-console.log(arequipa);
-var generaciones = function(){
-
-}
-
-// var sedes = Object.keys(data);
-
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
-
-// console.log(sedes);
-
-var generaciones = data.AQP; 
-// console.log(generaciones);
-
-var properties = Object.keys(generaciones);
-var valor = properties[0];
-
-
-// console.log(properties);
-
-var newArray = [
-	[valor, generaciones[valor]],
-	[properties[1], generaciones[properties[1]]]
-];
-
-
-var est = data.AQP["2016-2"].students[0];
-
-
-var activo = newArray[0][1].students[0].active;
-
-console.log(activo);
-
-console.log(newArray);
-// console.log(generaciones[properties[1]);
-
-
-
-// ----------------------- PUNTUACION PROMEDIO DE PROFESORES -----------------------
-
-function teachers(sede, generacion){
-	
-}
-
-
-
+entrar.addEventListener('click', function(){
+	window.location.href='overview.html';
+});
