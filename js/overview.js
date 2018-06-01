903e5f1ea2acd0402b63178b651581179c5720ec
antaresvalle <antaresvll561@gmail.com>
2018-06-01 11:16:46 -0500

    estructuracion de overview.js y se agrega clase para boton de menu activo

---
+++ js/overview.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function() {
+    console.log('All assets are loaded')
+})
+
+
+/*  ---------------- Obtencion de datos principales ---------------- */
+
+
+/*  ---------------- Cantidad de estudiantes activas ---------------- */
+
+
+/*  ---------------- % de estudiantes no activas x sede y x generacion ---------------- */
+
+
+/*  ---------------- % de estudiantes no activas x sede y x generacion ---------------- */
+
+
+/*  ---------------- Cantidad y porcentaje mayor que 70% x generacion ---------------- */
+
+
+	/*  ---------------- cantidad y porcentaje mayor a 70% : TECH ---------------- */
+
+
+	/*  ---------------- cantidad y porcentaje mayor a 70% : HSE ---------------- */
+
+
+/*  ---------------- Porcentaje estudiantes satisfechas ---------------- */
+
+
+/*  ---------------- Puntuacion promedio de Profesores ---------------- */
+
+
+/*  ---------------- Puntuacion promedio de las JediMasters ---------------- */
